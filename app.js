8bec14671bdd2fa6f79574034263d1ee911436be
Joshua <jcjoshuac@gmail.com>
2021-12-29 14:10:25 +0800

    set up web app for heroku deployment

--- app.js
+++ app.js
@@ -82,6 +82,6 @@ app.get("/about", function(req, res){
   res.render("about", {aboutContent: aboutContent});
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
